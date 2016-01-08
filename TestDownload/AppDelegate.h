ed9fb8aea52d1b54f42d441ae3973978056971cf
薛焱 <xueyan@xueyandeimac.local>
2016-01-08 17:22:25 +0800

    Initial Commit

---
+++ TestDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDownload
+//
+//  Created by 薛焱 on 16/1/8.
+//  Copyright © 2016年 薛焱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
